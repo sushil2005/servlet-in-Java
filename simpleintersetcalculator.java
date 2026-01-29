995ac797be9d3055852f93be0521e0f71e14de91
Sushil gaire <sushilgaire61@gmail.com>
2026-01-29 23:09:04 +0545

    Add files via upload

---
+++ simpleintersetcalculator.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class servlet03 extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        int p = Integer.parseInt(request.getParameter("principal"));
+        int t = Integer.parseInt(request.getParameter("time"));
+        int r = Integer.parseInt(request.getParameter("rate"));
+        
+        int si = (p * t * r) / 100;
+        
+        out.println("<h3>The Simple Interest is: " + si + "</h3>");
+        out.close();
+    }
+}
